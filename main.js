68f65e472e9fea43e1f54b2c84ea70320b8c171e
Jufebrown <jufebrown@gmail.com>
2017-03-09 15:07:15 -0600

    added array for customer table population

--- main.js
+++ main.js
@@ -85,14 +85,13 @@ db.run(`CREATE TABLE IF NOT EXISTS payment_options (payment_options_id INTEGER P
 // The product id
 db.run(`CREATE TABLE IF NOT EXISTS products (products_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name TEXT, product_price TEXT)`)
 
-// Create an array of at least 6 objects. Each object should have a key value pair matching each column name in the employees table.
-let employeeArray = [
-  {id: 0, firstName: 'Fred', lastName: 'Flintstone', jobTitle: 'Crane Operator', address: '1 Bedrock Way'},
-  {id: 0, firstName: 'Wilma', lastName: 'Flintstone', jobTitle: 'Homemaker', address: '1 Bedrock Way'},
-  {id: 0, firstName: 'Chloe', lastName: 'Catherine', jobTitle: 'Lounger', address: '513 Stacy Square'},
-  {id: 0, firstName: 'Sam', lastName: 'Gamgee', jobTitle: 'Mouser', address: '513 Stacy Square'},
-  {id: 0, firstName: 'Chuck', lastName: 'Doggert', jobTitle: 'Panter', address: '9970 Welty Rd'},
-  {id: 0, firstName: 'Matt', lastName: 'Smith', jobTitle: 'Doctor', address: '1 Milky Way'},
+// Array for customer table population
+let customerArray = [
+  {id: 0, firstName: 'Fred', lastName: 'Flintstone', address: '1 Bedrock Way', city: 'Bedrock', state: 'Pangaea', postcode: '37211', phone_number: '555-555-5555'},
+  {id: 0, firstName: 'Wilma', lastName: 'Flintstone', address: '1 Bedrock Way', city: 'Bedrock', state: 'Pangaea', postcode: '37211', phone_number: '555-555-5555'},
+  {id: 0, firstName: 'George', lastName: 'Burns', address: '2 Mulholland Drive', city: 'Los Angeles', state: 'CA', postcode: '15101', phone_number: '555-555-6666'},
+  {id: 0, firstName: 'Lucille', lastName: 'Ball', address: '64 Rodeo Dr', city: 'Beverly Hills', state: 'CA', postcode: '90210', phone_number: '555-555-7777'},
+  {id: 0, firstName: 'Desi', lastName: 'Arnez', address: '64 Rodeo Dr', city: 'Beverly Hills', state: 'CA', postcode: '90210', phone_number: '555-555-7777'},
 ]
 
 // Insert each of the employee objects into the database.
